8c912e5c50cf71c07567c62bb90c5f17624a423e
Amphoras <david@ravenswood.org.uk>
2011-10-17 21:52:43 +0200

    Update src/com.amphoras.tpthelper/Instructions.java

--- src/com.amphoras.tpthelper/Instructions.java
+++ src/com.amphoras.tpthelper/Instructions.java
@@ -35,7 +35,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class Instructions extends Activity {
-  private final int CHANGE_LOCALE = 1;
+	private final int CHANGE_LOCALE = 1;
 	SharedPreferences preferences;
 	
 	/** Called when the activity is first created. */
@@ -113,8 +113,10 @@ public class Instructions extends Activity {
           CharSequence german = getText(R.string.german);
           CharSequence russian = getText(R.string.russian);
           CharSequence chinese = getText(R.string.chinese);
+          CharSequence portuguese = getText(R.string.portuguese);
+          CharSequence spanish = getText(R.string.spanish);
           CharSequence cancel = getText(R.string.cancel);
-          final CharSequence[] locales = {english, french, german, russian, chinese, cancel};
+          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, cancel};
       	  localebuilder.setItems(locales, new DialogInterface.OnClickListener() {
       	    public void onClick(DialogInterface dialog, int item) {
       	    	Editor editlocale = preferences.edit();
@@ -155,6 +157,20 @@ public class Instructions extends Activity {
       	    	    Instructions.this.finish();
       	    		break;
       	    	case 5:
+      	    		editlocale.putString("locale", "pt");
+      	    		editlocale.commit();
+      	    		Intent n = new Intent(Instructions.this, HomeActivity.class);
+      	    	    startActivity(n);
+      	    	    Instructions.this.finish();
+      	    		break;
+      	    	case 6:
+      	    		editlocale.putString("locale", "es");
+      	    		editlocale.commit();
+      	    		Intent o = new Intent(Instructions.this, HomeActivity.class);
+      	    	    startActivity(o);
+      	    	    Instructions.this.finish();
+      	    		break;
+      	    	case 7:
       	    		// Do nothing
       	    		break;
       	    	}
