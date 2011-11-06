b9ed73344e1792cfcb59c8b5e22c43ddb06ad515
Amphoras <david@ravenswood.org.uk>
2011-11-06 22:27:06 +0000

    Update src/com.amphoras.tpthelper/About.java

--- src/com.amphoras.tpthelper/About.java
+++ src/com.amphoras.tpthelper/About.java
@@ -161,8 +161,9 @@ public class About extends Activity {
           CharSequence chinese = getText(R.string.chinese);
           CharSequence portuguese = getText(R.string.portuguese);
           CharSequence spanish = getText(R.string.spanish);
+          CharSequence serbian = getText(R.string.serbian);
           CharSequence cancel = getText(R.string.cancel);
-          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, cancel};
+          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, serbian, cancel};
       	  localebuilder.setItems(locales, new DialogInterface.OnClickListener() {
       	    public void onClick(DialogInterface dialog, int item) {
       	    	Editor editlocale = preferences.edit();
@@ -217,6 +218,13 @@ public class About extends Activity {
       	    	    About.this.finish();
       	    		break;
       	    	case 7:
+      	    		editlocale.putString("locale", "sr");
+      	    		editlocale.commit();
+      	    		Intent p = new Intent(About.this, HomeActivity.class);
+      	    	    startActivity(p);
+      	    	    About.this.finish();
+      	    		break;
+      	    	case 8:
       	    		// Do nothing
       	    		break;
       	    	}
