d877f696203118a5b0f322cef0e67c2e744be9ee
Amphoras <david@ravenswood.org.uk>
2011-11-06 22:25:56 +0000

    Update src/com.amphoras.tpthelper/EnterFile.java

--- src/com.amphoras.tpthelper/EnterFile.java
+++ src/com.amphoras.tpthelper/EnterFile.java
@@ -105,8 +105,9 @@ public class EnterFile extends Activity {
           CharSequence chinese = getText(R.string.chinese);
           CharSequence portuguese = getText(R.string.portuguese);
           CharSequence spanish = getText(R.string.spanish);
+          CharSequence serbian = getText(R.string.serbian);
           CharSequence cancel = getText(R.string.cancel);
-          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, cancel};
+          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, serbian, cancel};
       	  localebuilder.setItems(locales, new DialogInterface.OnClickListener() {
       	    public void onClick(DialogInterface dialog, int item) {
       	    	Editor editlocale = preferences.edit();
@@ -161,6 +162,13 @@ public class EnterFile extends Activity {
       	    	    EnterFile.this.finish();
       	    		break;
       	    	case 7:
+      	    		editlocale.putString("locale", "sr");
+      	    		editlocale.commit();
+      	    		Intent p = new Intent(EnterFile.this, HomeActivity.class);
+      	    	    startActivity(p);
+      	    	    EnterFile.this.finish();
+      	    		break;
+      	    	case 8:
       	    		// Do nothing
       	    		break;
       	    	}
