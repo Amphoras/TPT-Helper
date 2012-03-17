e38ae5794eb2b0110d6a56e26d48e8e88d400224
David Phillips <david@ravenswood.org.uk>
2012-03-17 22:04:41 +0000

    testing new languages

--- TPT Helper/src/com/amphoras/tpthelper/HomeActivity.java
+++ TPT Helper/src/com/amphoras/tpthelper/HomeActivity.java
@@ -700,8 +700,12 @@ public class HomeActivity extends ListActivity {
           CharSequence polish = getText(R.string.polish);
           CharSequence hungarian = getText(R.string.hungarian);
           CharSequence swedish = getText(R.string.swedish);
+          CharSequence italian = getText(R.string.italian);
+          CharSequence dutch_be = getText(R.string.dutch_be);
+          CharSequence portuguese_br = getText(R.string.portuguese_br);
+          CharSequence greek = getText(R.string.greek);
           CharSequence cancel = getText(R.string.cancel);
-          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, serbian, czech, polish, hungarian, swedish, cancel};
+          final CharSequence[] locales = {english, french, german, russian, chinese, portuguese, spanish, serbian, czech, polish, hungarian, swedish, italian, dutch_be, portuguese_br, greek, cancel};
       	  localebuilder.setItems(locales, new DialogInterface.OnClickListener() {
       	    public void onClick(DialogInterface dialog, int item) {
       	    	Editor editlocale = preferences.edit();
@@ -791,6 +795,34 @@ public class HomeActivity extends ListActivity {
       	    	    HomeActivity.this.finish();
       	    		break;
       	    	case 12:
+      	    		editlocale.putString("locale", "it");
+      	    		editlocale.commit();
+      	    		Intent u = new Intent(HomeActivity.this, HomeActivity.class);
+      	    	    startActivity(u);
+      	    	    HomeActivity.this.finish();
+      	    		break;
+      	    	case 13:
+      	    		editlocale.putString("locale", "nl");
+      	    		editlocale.commit();
+      	    		Intent v = new Intent(HomeActivity.this, HomeActivity.class);
+      	    	    startActivity(v);
+      	    	    HomeActivity.this.finish();
+      	    		break;
+      	    	case 14:
+      	    		editlocale.putString("locale", "pt_BR");
+      	    		editlocale.commit();
+      	    		Intent w = new Intent(HomeActivity.this, HomeActivity.class);
+      	    	    startActivity(w);
+      	    	    HomeActivity.this.finish();
+      	    		break;
+      	    	case 15:
+      	    		editlocale.putString("locale", "el");
+      	    		editlocale.commit();
+      	    		Intent x = new Intent(HomeActivity.this, HomeActivity.class);
+      	    	    startActivity(x);
+      	    	    HomeActivity.this.finish();
+      	    		break;
+      	    	case 16:
       	    		// Do nothing
       	    		break;
       	    	}
